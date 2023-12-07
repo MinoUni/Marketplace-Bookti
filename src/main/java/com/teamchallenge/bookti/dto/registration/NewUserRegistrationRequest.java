eee8f4d31e23b43cdf488370f0b3f62abbaee976
MinoUni <maks.reva.ua@gmail.com>
2023-12-07 16:04:05 +0200

    move to separate package, rename, remove redundant validation

--- src/main/java/com/teamchallenge/bookti/dto/registration/NewUserRegistrationRequest.java
+++ src/main/java/com/teamchallenge/bookti/dto/registration/NewUserRegistrationRequest.java
@@ -1,7 +1,9 @@
-package com.teamchallenge.bookti.registration;
+package com.teamchallenge.bookti.dto.registration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,28 +11,25 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 @Builder
-public class UserDto {
+public class NewUserRegistrationRequest {
+
     @JsonProperty("first_name")
     @NotBlank(message = "Field 'first_name' must not be blank")
-    @NotNull
     private String firstName;
 
     @JsonProperty("last_name")
     @NotBlank(message = "Field 'last_name' must not be blank")
-    @NotNull
     private String lastName;
 
     @NotBlank(message = "Field 'email' must not be blank")
     @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$",
             message = "Email must contain at least 1 character before '@' and domain name after '@'")
-    @NotNull
     private String email;
 
     @NotBlank(message = "Field 'password' must not be blank")
     @Size(min = 8, max = 20, message = "Password must be from 8 to 20 symbols length")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,20}$",
             message = "Password must contain minimum eight characters, at least one uppercase letter, one lowercase letter and one number")
-    @NotNull
     private String password;
 
     @JsonProperty("confirm_password")
@@ -38,14 +37,7 @@ public class UserDto {
     @Size(min = 8, max = 20, message = "Password must be from 8 to 20 symbols length")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,20}$",
             message = "Password must contain minimum eight characters, at least one uppercase letter, one lowercase letter and one number")
-    @NotNull
     private String confirmPassword;
-
-    public boolean confirmPassword(String password, String confirmPassword) {
-        return password.equals(confirmPassword);
-    }
-
-
 
 }
 
