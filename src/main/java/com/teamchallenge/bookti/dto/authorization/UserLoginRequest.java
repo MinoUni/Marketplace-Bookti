0e8770901afc1e00b7c8676bba50616048e6e2cf
MinoUni <maks.reva.ua@gmail.com>
2023-12-21 15:21:19 +0200

    Exclude redundant lombok generation, add swagger examples for <email>, <password>

--- src/main/java/com/teamchallenge/bookti/dto/authorization/UserLoginRequest.java
+++ src/main/java/com/teamchallenge/bookti/dto/authorization/UserLoginRequest.java
@@ -1,17 +1,21 @@
 package com.teamchallenge.bookti.dto.authorization;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class UserLoginRequest {
 
+    @Schema(type = "string", example = "mark.javar@gmail.com")
+    @NotBlank(message = "Field <email> must not be blank")
     private String email;
 
+    @Schema(type = "string", example = "Javard1rkk")
+    @Size(min = 8, max = 20, message = "Password must be from 8 to 20 symbols length")
+    @NotBlank(message = "Field <password> must not be blank")
     private String password;
 }
