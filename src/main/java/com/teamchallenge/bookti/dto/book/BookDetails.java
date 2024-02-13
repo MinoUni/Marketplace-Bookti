33d2eff34b15ccc7c497dec66916bc25be7b10e8
MinoUni <maks.reva.ua@gmail.com>
2024-02-13 15:26:48 +0200

    Add builder + jpa DTO constructor

--- src/main/java/com/teamchallenge/bookti/dto/book/BookDetails.java
+++ src/main/java/com/teamchallenge/bookti/dto/book/BookDetails.java
@@ -2,7 +2,9 @@ package com.teamchallenge.bookti.dto.book;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.teamchallenge.bookti.model.Book;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,6 +19,7 @@ import java.util.UUID;
  */
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Getter
 public class BookDetails {
 
@@ -44,4 +47,46 @@ public class BookDetails {
   private UUID userId;
 
   private String description;
+
+  public BookDetails(
+      UUID id,
+      String title,
+      String author,
+      String genre,
+      LocalDate publicationDate,
+      String language,
+      String tradeFormat,
+      String imageUrl,
+      String description) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.publicationDate = publicationDate;
+    this.language = language;
+    this.tradeFormat = tradeFormat;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
+
+  /**
+   * Method to map book entity into book DTO.
+   *
+   * @param book entity
+   * @return book DTO
+   */
+  public static BookDetails build(Book book) {
+    return BookDetails.builder()
+        .id(book.getId())
+        .title(book.getTitle())
+        .author(book.getAuthor())
+        .genre(book.getGenre())
+        .publicationDate(book.getPublicationDate())
+        .language(book.getLanguage())
+        .tradeFormat(book.getTradeFormat())
+        .imageUrl(book.getImageUrl())
+        .userId(book.getOwner().getId())
+        .description(book.getDescription())
+        .build();
+  }
 }
