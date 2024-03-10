1c9b93fd40f8b8f2dfb130d21357940c24da2e74
MinoUni <maks.reva.ua@gmail.com>
2024-03-10 16:31:56 +0200

    Fix checkstyle in BookProfile.java

--- src/main/java/com/teamchallenge/bookti/book/BookProfile.java
+++ src/main/java/com/teamchallenge/bookti/book/BookProfile.java
@@ -3,10 +3,9 @@ package com.teamchallenge.bookti.book;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.UUID;
-
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
