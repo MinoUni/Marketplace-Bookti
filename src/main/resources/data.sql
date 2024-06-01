631d81aa3375e0f79ab8683f79c5747126b095cc
MinoUni <maks.reva.ua@gmail.com>
2024-06-01 16:51:11 +0300

    format sql requests for better reading

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,34 +1,45 @@
 INSERT
-INTO users
-(full_name, email, password, avatar_url, location, rating, telegram_id, role, display_email, display_telegram)
-VALUES ('Peter Berger', 'peter.berger@testmail.com', '$2a$10$cFNxO/yS0zBfPhtnFQ779Od3LiMoqSG0zrh7.Pb98onbi4Oajwwqy',
-        null, 'Lviv', 3, null, 'ROLE_USER', false, false),
-       ('Laura Splice', 'laura.splice@testmail.com', '$2a$10$cFNxO/yS0zBfPhtnFQ779Od3LiMoqSG0zrh7.Pb98onbi4Oajwwqy',
-        null, 'Dnipro', 4, null, 'ROLE_USER', false, false),
-       ('Amiya Shiro', 'amiya.shiro@testmail.com', '$2a$10$cFNxO/yS0zBfPhtnFQ779Od3LiMoqSG0zrh7.Pb98onbi4Oajwwqy', null,
-        'Kharkiv', 3, null, 'ROLE_USER', false, false),
-       ('Saria Darkoff', 'saria.darkoff@testmail.com', '$2a$10$cFNxO/yS0zBfPhtnFQ779Od3LiMoqSG0zrh7.Pb98onbi4Oajwwqy',
-        null, 'Kyiv', 3, null, 'ROLE_USER', false, false);
+INTO users (
+    full_name,
+    email,
+    password,
+    avatar_url,
+    location,
+    rating,
+    telegram_id,
+    role,
+    display_email,
+    display_telegram)
+VALUES
+    ('Peter Berger',  'peter.berger@testmail.com',  '$2a$10$cFNxO/yS0zBfPhtnFQ779Od3LiMoqSG0zrh7.Pb98onbi4Oajwwqy', null, 'Lviv',    3, null, 'ROLE_USER',  false, false),
+    ('Laura Splice',  'laura.splice@testmail.com',  '$2a$10$cFNxO/yS0zBfPhtnFQ779Od3LiMoqSG0zrh7.Pb98onbi4Oajwwqy', null, 'Dnipro',  4, null, 'ROLE_USER',  false, false),
+    ('Amiya Shiro',   'amiya.shiro@testmail.com',   '$2a$10$cFNxO/yS0zBfPhtnFQ779Od3LiMoqSG0zrh7.Pb98onbi4Oajwwqy', null, 'Kharkiv', 3, null, 'ROLE_USER',  false, false),
+    ('Saria Darkoff', 'saria.darkoff@testmail.com', '$2a$10$cFNxO/yS0zBfPhtnFQ779Od3LiMoqSG0zrh7.Pb98onbi4Oajwwqy', null, 'Kyiv',    3, null, 'ROLE_USER',  false, false),
+    ('Admin',         'admin.bookti@testmail.com',  '$2a$10$Ir2Mm0HKzHmlZZXfagNnV.9cNfXAeeJV47sckzQXGLoQYnG4albMa', null, 'Kyiv',    0, null, 'ROLE_ADMIN', false, false);
 
 INSERT
-INTO books
-(owner_id, title, author, genre, description, image_name, image_url, language, publication_year, exchange_format)
-VALUES (1, 'The Great Gatsby', 'F.Scott Fitzgerald', 'fiction', 'Description...', null, null, 'english', 2024, 'gift'),
-       (1, 'Golden Fool', 'Robin Hobb', 'fiction', 'Description...', null, null, 'english', 2024, 'exchange'),
-       (2, 'Seven Blades in Black', 'Sam Sykes', 'fiction', 'Description...', null, null, 'english', 2024, 'exchange'),
-       (3, 'One Hundred Years of Solitude', 'Gabriel García Márquez', 'fiction', 'Description...', null, null,
-        'english', 2024, 'exchange'),
-       (2, 'Love in the Time of Cholera', 'Gabriel García Márquez', 'fiction', 'Description...', null, null, 'english',
-        2024, 'gift'),
-       (4, 'American Prometheus: The Inspiration', 'Kai Bird, Martin J. Sherwin', 'fiction', 'Description...', null,
-        null, 'english', 2024, 'gift'),
-       (4, 'The 48 Lows of Power', 'Robert Greene', 'fiction', 'Description...', null, null, 'english', 2024,
-        'exchange'),
-       (1, 'Can`t Hurt Me: Master Your Mind and Defy the Odds', 'David Goggins', 'fiction', 'Description...', null,
-        null, 'english', 2024, 'exchange'),
-       (1, 'The Woman in Me', 'Britney Spears', 'fiction', 'Description...', null, null, 'english', 2024, 'gift'),
-       (3, 'Jackie: Public, Private, Secret', 'J, Randy Taraborrelli', 'fiction', 'Description...', null, null,
-        'english', 2024, 'exchange');
+INTO books (
+    owner_id,
+    title,
+    author,
+    genre,
+    description,
+    image_name,
+    image_url,
+    language,
+    publication_year,
+    exchange_format)
+VALUES
+    (1, 'The Great Gatsby',                                  'F.Scott Fitzgerald',          'fiction', 'Description...', null, null, 'english', 2024, 'gift'),
+    (1, 'Golden Fool',                                       'Robin Hobb',                  'fiction', 'Description...', null, null, 'english', 2024, 'exchange'),
+    (2, 'Seven Blades in Black',                             'Sam Sykes',                   'fiction', 'Description...', null, null, 'english', 2024, 'exchange'),
+    (3, 'One Hundred Years of Solitude',                     'Gabriel García Márquez',      'fiction', 'Description...', null, null, 'english', 2024, 'exchange'),
+    (2, 'Love in the Time of Cholera',                       'Gabriel García Márquez',      'fiction', 'Description...', null, null, 'english', 2024, 'gift'),
+    (4, 'American Prometheus: The Inspiration',              'Kai Bird, Martin J. Sherwin', 'fiction', 'Description...', null, null, 'english', 2024, 'gift'),
+    (4, 'The 48 Lows of Power',                              'Robert Greene',               'fiction', 'Description...', null, null, 'english', 2024, 'exchange'),
+    (1, 'Can`t Hurt Me: Master Your Mind and Defy the Odds', 'David Goggins',               'fiction', 'Description...', null, null, 'english', 2024, 'exchange'),
+    (1, 'The Woman in Me',                                   'Britney Spears',              'fiction', 'Description...', null, null, 'english', 2024, 'gift'),
+    (3, 'Jackie: Public, Private, Secret',                   'J, Randy Taraborrelli',       'fiction', 'Description...', null, null, 'english', 2024, 'exchange');
 
 
 INSERT
